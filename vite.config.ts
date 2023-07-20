c3132874e58d59aa7c68c813aa18d7d821bbd30e
Umbraco Cloud <support@umbraco.io>
2023-07-20 17:24:56 +0000

    Deployment dac1bd8f-1ec1-47fb-8e0a-7472b812439c: Run number 28

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    outDir: 'src/MyAwesomeProject.Web/wwwroot/app/', // inside the Umbraco project
+    emptyOutDir: true, // needs to be explicitly set because it’s outside of ./
+    rollupOptions: {
+      input: {
+        main: resolve( 'src/js/main.ts')
+      }
+    }
+  }
+})
