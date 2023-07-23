75b9aa90461325262567a68bfa9a9ff2b070d3ad
Jesper Madsen <jam@umbraco.dk>
2023-07-23 14:00:30 +0200

    more info

--- scripts/upload_package.sh
+++ scripts/upload_package.sh
@@ -8,12 +8,15 @@ apiKey="$3"
 file="$4"
 
 function call_api {
-  response=$(curl -s -X POST $url \
+  response=$(curl -i -s -X POST $url \
     -H "Umbraco-Api-Key: $apiKey" \
     -H "Content-Type: multipart/form-data" \
     --form "file=@$file")
-
+  
+  echo "$file"
+  
   echo "$response"
+  
 }
 
 call_api
