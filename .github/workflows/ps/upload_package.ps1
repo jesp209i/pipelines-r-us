21599e7caa603a260f189c84bdee733c25608c1d
Jesper Madsen <jam@umbraco.dk>
2023-10-09 10:43:49 +0200

    a bit error handling

--- .github/workflows/ps/upload_package.ps1
+++ .github/workflows/ps/upload_package.ps1
@@ -12,20 +12,24 @@ $FieldName = 'file'
 $ContentType = 'application/zip'
 $UmbracoHeader = @{ 'Umbraco-Cloud-Api-Key' = $ApiKey }
 
-$FileStream = [System.IO.FileStream]::new($FilePath, [System.IO.FileMode]::Open)
-$FileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new('form-data')
-$FileHeader.Name = $FieldName
-$FileHeader.FileName = Split-Path -leaf $FilePath
-$FileContent = [System.Net.Http.StreamContent]::new($FileStream)
-$FileContent.Headers.ContentDisposition = $FileHeader
-$FileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse($ContentType)
-#$FileContent.Headers.Add($UmbracoHeader)
+function Upload-Package {
+    $FileStream = [System.IO.FileStream]::new($FilePath, [System.IO.FileMode]::Open)
+    $FileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new('form-data')
+    $FileHeader.Name = $FieldName
+    $FileHeader.FileName = Split-Path -leaf $FilePath
+    $FileContent = [System.Net.Http.StreamContent]::new($FileStream)
+    $FileContent.Headers.ContentDisposition = $FileHeader
+    $FileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse($ContentType)
 
-$MultipartContent = [System.Net.Http.MultipartFormDataContent]::new()
-$MultipartContent.Add($FileContent)
+    $MultipartContent = [System.Net.Http.MultipartFormDataContent]::new()
+    $MultipartContent.Add($FileContent)
 
-#Write-Host $FileContent.Headers
+    $Response = Invoke-WebRequest -Body $MultipartContent -Headers $UmbracoHeader  -Method 'POST' -Uri $Url
+    if ($Response.StatusCode -ne 202)
+    {
+        throw "Error during upload"
+    }
+    Write-Host $Response.Content | ConvertTo-Json
+}
 
-$Response = Invoke-WebRequest -Body  $MultipartContent -Headers $UmbracoHeader  -Method 'POST' -Uri $Url
-
-Write-Host $Response
+Upload-Package
