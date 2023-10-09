6427bd73ed517a47da7484989b6fae1637a46415
Jesper Madsen <jam@umbraco.dk>
2023-10-09 09:28:09 +0200

    header

--- .github/workflows/ps/upload_package.ps1
+++ .github/workflows/ps/upload_package.ps1
@@ -9,7 +9,8 @@ param(
 $Url = "$BaseUrl/v1/projects/$ProjectId/deployments/$DeploymentId/package"
 
 $FieldName = 'file'
-$ContentType = 'application/json'
+$ContentType = 'application/zip'
+$UmbracoHeader = @{ 'Umbraco-Cloud-Api-Key' = $ApiKey }
 
 $FileStream = [System.IO.FileStream]::new($FilePath, [System.IO.FileMode]::Open)
 $FileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new('form-data')
@@ -18,11 +19,13 @@ $FileHeader.FileName = Split-Path -leaf $FilePath
 $FileContent = [System.Net.Http.StreamContent]::new($FileStream)
 $FileContent.Headers.ContentDisposition = $FileHeader
 $FileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse($ContentType)
-$FileContent.Headers.Add("Umbraco-Api-Key", $ApiKey)
+#$FileContent.Headers.Add($UmbracoHeader)
 
 $MultipartContent = [System.Net.Http.MultipartFormDataContent]::new()
 $MultipartContent.Add($FileContent)
 
-$Response = Invoke-WebRequest -Body $MultipartContent -Method 'POST' -Uri $Url
+#Write-Host $FileContent.Headers
+
+$Response = Invoke-WebRequest -Body  $MultipartContent -Headers $UmbracoHeader  -Method 'POST' -Uri $Url
 
 Write-Host $Response
