a7fd62ec75c0705af76f7102b0982e34106c8a9e
Jesper Madsen <jesper.a.madsen@gmail.com>
2023-12-07 17:07:19 +0100

    private nuget

--- src/MyAwesomeProject.Code/Class1.cs
+++ src/MyAwesomeProject.Code/Class1.cs
@@ -1,7 +1,13 @@
-﻿namespace MyAwesomeProject.Code;
+﻿using Umbraco.Cloud.Shared.Kernel;
+namespace MyAwesomeProject.Code;
 
 public class Class1
 {
+ public Class1()
+ {
+  var plan = GlobalPlan.CloudStarter; 
+ }
+ 
  // code change
  // using new project-id based api for the first time
 }
