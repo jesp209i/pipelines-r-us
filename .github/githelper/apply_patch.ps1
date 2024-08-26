ef0c577ec78474a5f511a1f7f45cfc78da5e257c
Jesper Madsen <jesper.a.madsen@gmail.com>
2024-08-26 20:49:10 +0200

    try to use pipelinevendor

--- .github/githelper/apply_patch.ps1
+++ .github/githelper/apply_patch.ps1
@@ -1,29 +1,67 @@
 param (
-    [string]$patchFile,
-    [string]$latestDeploymentId,
-    [string]$gitUserName = "github-actions",
-    [string]$gitUserEmail = "github-actions@github.com"
+    [Parameter(Position=0)]
+    [string]
+    $PatchFile,
+    
+    [Parameter(Position=1)]
+    [string]
+    $LatestDeploymentId,
+    
+    [Parameter(Position=3)]
+    [string]
+    $PipelineVendor,
+
+    [Parameter(Position=3)]
+    [string]
+    $GitUserName = "github-actions",
+    
+    [Parameter(Position=4)]
+    [string]
+    $GitUserEmail = "github-actions@github.com"
 )
 
 
-git config user.name $gitUserName
-git config user.email $gitUserEmail
+git config user.name $GitUserName
+git config user.email $GitUserEmail
 
 # Check if the patch has been applied already, skip if it has
-if (git apply $patchFile --reverse --ignore-space-change --ignore-whitespace --check) {
-    Write-Output "Patch already applied, exit"
+if (git apply $PatchFile --reverse --ignore-space-change --ignore-whitespace --check) {
+    Write-Output "Patch already applied => concluding the apply patch part"
     exit 0
 }
 # Check if the patch can be applied
-elseif (git apply $patchFile --ignore-space-change --ignore-whitespace --check) {
+elseif (git apply $PatchFile --ignore-space-change --ignore-whitespace --check) {
     Write-Output "Patch needed, trying now"
-    git apply $patchFile --ignore-space-change --ignore-whitespace
+    git apply $PatchFile --ignore-space-change --ignore-whitespace
     git add *
-    git commit -m "Adding cloud changes since deployment $latestDeploymentId [skip ci]"
+    git commit -m "Adding cloud changes since deployment $LatestDeploymentId [skip ci]"
     git push
     # Record the new sha for the deploy
     $updatedSha = git rev-parse HEAD
     Write-Output "Updated SHA: $updatedSha"
-    Add-Content -Path $env:GITHUB_OUTPUT -Value "updatedSha=$updatedSha"
+
+    switch ($PipelineVendor) {
+        "GITHUB" {
+            "updatedSha=$($updatedSha)" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+        }
+        "AZUREDEVOPS" {
+            Write-Host "##vso[task.setvariable variable=updatedSha;]$($updatedSha)"
+        }
+        "TESTRUN" {
+            Write-Host $PipelineVendor
+        }
+        Default {
+            Write-Host "Please use one of the supported Pipeline Vendors or enhance script to fit your needs"
+            Write-Host "Currently supported are: GITHUB and AZUREDEVOPS"
+            Exit 1
+        }
+    }
+}
+else {
+
+    Write-Output "Patch cannot be applied - please check the output below for the problematic parts"
+    Write-Output "================================================================================="
+    git apply --reject $PatchFile --ignore-space-change --ignore-whitespace --check
+    exit 1
 }
 
--- .github/githelper/apply_patch.sh
+++ .github/githelper/apply_patch.sh
@@ -3,10 +3,11 @@
 # Set required variables
 patchFile="$1"
 latestDeploymentId="$2"
+pipelineVendor="$3"
 
 # Not required, defaults to:
-gitUserName="$3" # github-actions
-gitUserEmail="$4"  # github-actions@github.com
+gitUserName="$4" # github-actions
+gitUserEmail="$5"  # github-actions@github.com
 
 if [[ -z "$gitUserName" ]]; then
     gitUserName="github-actions"
@@ -20,7 +21,7 @@ git config user.name "$gitUserName"
 git config user.email "$gitUserEmail"
 # Check if the patch has been applied already, skip if it has
 if git apply "$patchFile" --reverse --ignore-space-change --ignore-whitespace --check; then
-    echo "Patch already applied, exit"
+    echo "Patch already applied => concluding the apply patch part"
     exit 0
 # check if the patch can be applied
 elif git apply "$patchFile" --ignore-space-change --ignore-whitespace --check; then
@@ -32,5 +33,23 @@ elif git apply "$patchFile" --ignore-space-change --ignore-whitespace --check; t
     # record the new sha for the deploy
     updatedSha=$(git rev-parse HEAD)
     echo "Updated SHA: $updatedSha"
-    echo "updatedSha=$updatedSha" >> "$GITHUB_OUTPUT"
+    
+    ## Write the updated Sha to the pipelines variables for use in a later step
+    if [[ "$pipelineVendor" == "GITHUB" ]]; then
+        echo "updatedSha=$updatedSha" >> "$GITHUB_OUTPUT"
+        exit 0
+    elif [[ "$pipelineVendor" == "AZUREDEVOPS" ]]; then
+        echo "##vso[task.setvariable variable=updatedSha;isOutput=true]$updatedSha"
+        exit 0
+    elif [[ "$pipelineVendor" == "TESTRUN" ]]; then
+        echo $pipelineVendor
+        exit 0
+    fi
+
+    # Handle the case where the patch cannot be applied
+else
+    echo "Patch cannot be applied - please check the output below for the problematic parts"
+    echo "================================================================================="
+    git apply --reject "$patchFile" --ignore-space-change --ignore-whitespace --check
+    exit 1
 fi
